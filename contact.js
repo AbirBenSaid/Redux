d325c0ecc5e3f4f0199588621c20b4d617c4faef
AbirBenSaid <abirbensaid96@gmail.com>
2021-04-13 13:03:51 +0100

    first commit

---
+++ contact.js
@@ -0,0 +1,18 @@
+const contacts = [
+    {
+      name: "Samy Sghaier",
+      imgURL:
+        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      phone: "+216 21 009 458",
+      email: "Samypaypall@gmail.com"
+    },
+    {
+      name: "Abir Ben Said",
+      imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+216 ",
+      email: "abir@bensaid.com"
+    }
+  ];
+  
+  export default contacts;
